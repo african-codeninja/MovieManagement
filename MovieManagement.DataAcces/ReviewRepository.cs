c2ce39ea09e7abf04ed41dff0f746f6e99d04856
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-03 10:30:42 -0400

    Create ReviewRepository.cs
    Creating reveiwRepository controllers with methods

---
+++ MovieManagement.DataAcces/ReviewRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.DataAcces
+{
+    public class ReviewRepository : BaseRepository
+    {
+        public List<Review> searchReviews()
+        {
+            return DbContext.Reviews.ToList();
+        }
+        //get method
+        public Review GetReview(Guid reviewId)
+        {
+            var review = DbContext.Reviews.FirstOrDefault(a => a.Id == reviewId);
+            return review;
+        }
+
+        //post method add category
+        public void AddReview(Review newReview)
+        {
+            DbContext.Reviews.Add(newReview);
+            DbContext.SaveChanges();
+        }
+
+        //delete category method
+        public void DeleteReview(Guid reviewId)
+        {
+            var review = DbContext.Reviews.FirstOrDefault(a => a.Id == reviewId);
+
+            if (review != null)
+            {
+                DbContext.Reviews.Remove(review);
+                DbContext.SaveChanges();
+            }
+
+        }
+
+        public void updateReview(Review updatedReview)
+        {
+            var existingReview = DbContext.Reviews.FirstOrDefault(a => a.Id == updatedReview.Id);
+            if (existingReview != null)
+            {
+                existingReview.Score = updatedReview.Score;
+                DbContext.SaveChanges();
+            }
+        }
+    }
+}
