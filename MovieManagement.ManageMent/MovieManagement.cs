20f2e07e0d694588b29d10eb0feff1345bec9fdd
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-03 10:28:17 -0400

    Create MovieManagement.cs
    created Reveiw DTO class

---
+++ MovieManagement.ManageMent/MovieManagement.cs
@@ -0,0 +1,38 @@
+﻿using MovieManagement.DataAcces;
+using MovieManagement.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.ManageMent
+{
+    public class MovieManagement
+    {
+        //Serach list
+        public List<MovieDTO> search()
+        {
+            var repo = new MovieRepository();
+            var result = repo.searchMovies();
+
+            var toReturn = result.Select(a => new MovieDTO
+            {
+                Id = a.Id,
+                AverageScore = (float)a.AverageScore,
+                CategoryName = a.Category.Name,
+                Length = a.Length,
+                Rating = a.Rating,
+                ReleaseDate = a.ReleaseDate,
+                Title = a.Title
+            }).ToList();
+            return toReturn;
+        }
+
+        //Get Method
+        public MovieDTO GetMovie(Guid id)
+        {
+            var repo = new 
+        }
+    }
+}
